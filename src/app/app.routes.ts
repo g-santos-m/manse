ef27d5d988bfdc31fc764772675612ffd34e2a6f
mgarcia <garcia.pires.1998@hotmail.es>
2025-11-21 20:22:40 +0100

    añadir listado parte a la barra de nav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CrearParte } from './features/component/crear-parte/crear-parte';
 import { DetalleParte } from './features/component/detalle-parte/detalle-parte';
+import { ListadoPartes } from './features/component/listado-partes/listado-partes'
 
 export const routes: Routes = [
   { path: 'crear-parte', component: CrearParte },
   { path: 'detalle-parte', component: DetalleParte },
+  { path: 'listado-partes', component: ListadoPartes},
 
-  { path: '', redirectTo: '/crear-parte', pathMatch: 'full' },
+  { path: '', redirectTo: '/listado-partes', pathMatch: 'full' },
   
-  { path: '**', redirectTo: '/crear-parte' }
+  { path: '**', redirectTo: '/listado-partes' }
 ];
 
 @NgModule({

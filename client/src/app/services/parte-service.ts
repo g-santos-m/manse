9d147d8f1ba0e5b98a93aed9d375dea08fec02ae
miguel <garcia.pires.1998@hotmail.es>
2025-12-06 11:49:43 +0100

    retoques codigo

--- client/src/app/services/parte-service.ts
+++ client/src/app/services/parte-service.ts
@@ -5,27 +5,21 @@ import { Observable } from "rxjs";
 
 @Injectable({providedIn: 'root'})
 export class ParteService {
-    // Asegúrate de que esta URL es correcta (ej: localhost:3000/api o localhost:8080/api)
     private apiUrl = 'http://localhost:3000/api';
     private http = inject(HttpClient);
 
-    // GET: Obtener todos
     getPartes = (): Observable<ApiResponse<Parte[]>> => {
         return this.http.get<ApiResponse<Parte[]>>(`${this.apiUrl}/partes`)
     };
 
-    // POST: Crear uno nuevo
     createParte = (parte: Parte): Observable<ApiResponse<Parte>> => {
         return this.http.post<ApiResponse<Parte>>(`${this.apiUrl}/parte`, parte);
     };
 
-    // PUT: Actualizar uno existente
-    // CORRECCIÓN: Si /parte/ID da 404, revertimos a /parte y enviamos el ID dentro del objeto
     updateParte = (parte: Parte): Observable<ApiResponse<Parte>> => {
         return this.http.put<ApiResponse<Parte>>(`${this.apiUrl}/parte`, parte);
     };
 
-    // DELETE: Borrar
     deleteParte = (id: number) => {
         return this.http.delete(`${this.apiUrl}/parte/${id}`);
     }
